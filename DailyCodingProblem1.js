d0cdc7ec0b8bfa43e71956b99f3c4518bbf87117
Filippos Makridis <philippos.makridis@gmail.com>
2021-11-22 22:56:40 +0200

    Cleaned the code with best practices

--- DailyCodingProblem1.js
+++ DailyCodingProblem1.js
@@ -1,15 +1,14 @@
 // Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
 // For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 
-const array1 = [10,15,3,7,14];
+const array1 = [10, 15, 3, 7, 14];
 let k = 17;
 
-for(let i of array1) {
-    let diff = k - i;
-    if(array1.includes(diff)) {
-        return true;
-    }
-    else {
-        return false;
-    }
+for (let i of array1) {
+  let diff = k - i;
+  if (array1.includes(diff)) {
+    return true;
+  } else {
+    return false;
+  }
 }
--- HardInterviewQuestion.js
+++ HardInterviewQuestion.js
@@ -1,17 +1,17 @@
 const array1 = [10, 15, 30];
 const array2 = [12, 15, 20];
 const array3 = [17, 20, 32];
-const mergedArray = array1.concat(array2,array3);
+const mergedArray = array1.concat(array2, array3);
 console.log(mergedArray);
 
-for(let i = 0; i < mergedArray.length-1; i++) {
-    for(let j = 0; j < mergedArray.length-1; j++) {
-        if(mergedArray[j] > mergedArray[j + 1]) {
-            let temp = mergedArray[j];
-            mergedArray[j] = mergedArray[j + 1];
-            mergedArray[j + 1] = temp;
-        }
+for (let i = 0; i < mergedArray.length - 1; i++) {
+  for (let j = 0; j < mergedArray.length - 1; j++) {
+    if (mergedArray[j] > mergedArray[j + 1]) {
+      let temp = mergedArray[j];
+      mergedArray[j] = mergedArray[j + 1];
+      mergedArray[j + 1] = temp;
     }
+  }
 }
 
 console.log(mergedArray);
